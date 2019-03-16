334d8d768316638e78d338a970c01802e6e98ef8
harrisoncrazy <harrisoncrawford16@yahoo.ca>
2019-03-16 14:55:24 -0400

    scaleable scrollview

--- Assets/Scripts/Building/BuildMaster.cs
+++ Assets/Scripts/Building/BuildMaster.cs
@@ -31,6 +31,7 @@ public class BuildMaster : MonoBehaviour
 {
     [SerializeField] private TextAsset jsonObj;
 
+    //HAVE TO USE A CONTAINER IN ORDER TO CORRECTLY PARSE A JSON FILE
     public infoListContainer myPartList = new infoListContainer();
     public List<imgReference> partImages;
 
@@ -38,7 +39,9 @@ public class BuildMaster : MonoBehaviour
 
     public void Start()
     {
+        //parsing JSON to string, data path
         string jsonText = File.ReadAllText(Application.dataPath + "/Test.json");
+        //String with JSON included, container with the class inside
         JsonUtility.FromJsonOverwrite(jsonText, myPartList);
 
         GenerateUI();
@@ -49,19 +52,33 @@ public class BuildMaster : MonoBehaviour
         int offset = 0;
         for (int i = 0; i < myPartList.partInfoList.Count; i++)
         {
+            //instanciating ui element, parenting it to the container
             GameObject prefab = Instantiate(uiDisplayPref, transform.position, transform.rotation) as GameObject;
             prefab.transform.SetParent(GameObject.Find("ObjectContainer").transform, false);
             prefab.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -100 - offset);
             offset += 160;
 
+            float scaleY = 180;
+
             for (int z = 0; z < partImages.Count; z++)
             {
                 if (myPartList.partInfoList[i].imgName == partImages[z].refName)
                 {
+                    //Setting the sprite and text on the ui element
                     prefab.GetComponent<partUIElement>().mainImage.sprite = partImages[z].partSprite;
                     prefab.GetComponent<partUIElement>().mainText.text = myPartList.partInfoList[i].displayName;
                 }
+
+                //Scaling the viewport/scroll area
+                scaleY += 160;
+                Vector2 newScale = new Vector2(GameObject.Find("ObjectContainer").GetComponent<RectTransform>().sizeDelta.x, scaleY);
+                GameObject.Find("ObjectContainer").GetComponent<RectTransform>().sizeDelta = newScale;
             }
+
+            //Scaling the viewport/scroll area (undoing single offset, as it goes over by 1 object) HACK SOLUTION, maybe find a way to fix later
+            scaleY -= 140; //160 - 20 for the small gap at the bottom
+            Vector2 newScale2 = new Vector2(GameObject.Find("ObjectContainer").GetComponent<RectTransform>().sizeDelta.x, scaleY);
+            GameObject.Find("ObjectContainer").GetComponent<RectTransform>().sizeDelta = newScale2;
         }
     }
 }
--- Assets/Scripts/Building/partUIElement.cs
+++ Assets/Scripts/Building/partUIElement.cs
@@ -17,6 +17,7 @@ public class partUIElement : MonoBehaviour
 
     void TaskOnClick()
     {
+        //Instanciating floating part
         GameObject prefab = Instantiate(floatingPartPref, transform.position, transform.rotation) as GameObject;
         prefab.transform.SetParent(GameObject.Find("Canvas").transform, false);
         prefab.transform.localPosition = new Vector3(0, 0);
